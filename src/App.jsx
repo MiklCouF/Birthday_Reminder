6ec727e0931e09948f9381a0777b5e1a2a7351c3
Michaël Couffignal <michael@couffignal.fr>
2024-05-20 12:16:48 +0200

    adding header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <h1>App</h1>
+      <Footer />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ function Header() {
     return (
 
         <header className="header">
+            <input type="text" id="username" name="username" />
+            <input type="password" id="pass" name="password" minlength="8" required />
+            <input className="connect" type="submit" value="Se connecter" />
             <h2>Header</h2>
         </header>
 
