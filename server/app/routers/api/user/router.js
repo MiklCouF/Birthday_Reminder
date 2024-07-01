331d67e7c1253b37b45c7e091bba899fd356506f
Michaël Couffignal <michael@couffignal.fr>
2024-07-01 17:03:08 +0200

    hach password and adding argon2 and cors library

--- server/app/routers/api/user/router.js
+++ server/app/routers/api/user/router.js
@@ -8,16 +8,19 @@ const router = express.Router();
 
 // Import user-related actions
 const { browse, read, add } = require("../../../controllers/userActions");
+const {
+  credentialsValidation,
+} = require("../../../services/credentialValidation");
+const { hashPassword } = require("../../../controllers/authActions");
 
 // Route to get a list of users
 router.get("/", browse);
-
 
 // Route to get a specific user by ID
 router.get("/:id", read);
 
 // Route to add a new user
-router.post("/", add);
+router.post("/", credentialsValidation, hashPassword, add);
 
 /* ************************************************************************* */
 
