07d9abca148f910cb565e96e30e8cc4ed3cd87f1
Michaël Couffignal <michael@couffignal.fr>
2025-04-18 13:55:27 +0200

    style(Search) change style

--- client/src/components/SearchComponent.jsx
+++ client/src/components/SearchComponent.jsx
@@ -66,19 +66,33 @@ const SearchComponent = () => {
   }, []);
 
   return (
-    <div id="search-container" style={{ position: "relative", width: "300px" }}>
+    <div
+      id="search-container"
+      style={{
+        width: "200px",
+        padding: "8px",
+        boxSizing: "border-box",
+        borderRadius: "12px",
+        border: "1px solid #ccc",
+        background: "white",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="20px"
+        viewBox="0 -960 960 960"
+        width="20px"
+        fill="#5F6368"
+        style={{ marginTop: "2px", marginRight: "2px" }}
+      >
+        <path d="M720-180 530.46-388.16q-30 24.77-69 38.77-39 14-80.69 14-102.55 0-173.58-71.01-71.03-71.01-71.03-173.54 0-102.52 71.01-173.6 71.01-71.07 173.54-71.07 102.52 0 173.6 71.03 71.07 71.03 71.07 173.58 0 42.85-14.38 81.85-14.39 39-38.39 67.84l200 200-42.15 42.16ZM380.77-395.38q77.31 0 130.96-53.66 53.66-53.65 53.66-130.96t-53.66-130.96q-53.65-53.66-130.96-53.66t-130.96 53.66Q196.15-657.31 196.15-580t53.66 130.96q53.65 53.66 130.96 53.66Z" />
+      </svg>
       <input
         type="text"
         placeholder="Rechercher..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        style={{
-          width: "100%",
-          padding: "8px",
-          boxSizing: "border-box",
-          borderRadius: "4px",
-          border: "1px solid #ccc",
-        }}
+        style={{ border: "none", outline: "none", width: "100%" }}
       />
 
       {query && filteredResults.length > 0 && (
