32a2213b8f779760a0f56fd0bb01c8ef630bf9f8
Michaël Couffignal <michael@couffignal.fr>
2025-04-24 18:45:20 +0200

    fix(addData) delete svg

--- client/src/components/AutocompleteLastnames.jsx
+++ client/src/components/AutocompleteLastnames.jsx
@@ -174,20 +174,6 @@ export default function AutocompleteLastnames({
         }}
         onClick={() => inputRef.current?.focus()}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-          style={{ marginRight: 8 }}
-        >
-          <path
-            fillRule="evenodd"
-            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-          />
-        </svg>
-
         <input
           ref={inputRef}
           type="text"
